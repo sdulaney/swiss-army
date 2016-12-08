9eebf3fd66fee97a3d97debd367e826f24a8efbb
Stewart Dulaney <sdulaney@users.noreply.github.com>
2016-12-08 04:08:41 -0800

    Update files

--- actions/batch_emails.js
+++ actions/batch_emails.js
@@ -2,10 +2,22 @@
 var _ = require('underscore');
 var fs = require('fs');
 
+// from thatsthem_emails
+var cheerio = require('cheerio');
+// Set up cached-request
+var request = require('request')
+,   cachedRequest = require('cached-request')(request)
+,   cacheDirectory = "./cache";
+cachedRequest.setCacheDirectory(cacheDirectory);
+cachedRequest.setValue('ttl', 157784760000);    // ttl in milliseconds, 5 years
+
+
+
+
 module.exports = function (phantomInstance, file) {
 
     // Email column
-    var emails = [];
+    var email_column = [];
 
     if(typeof require !== 'undefined') var XLSX = require('xlsx');
     var workbook = XLSX.readFile( './files/' + file );
@@ -18,7 +30,7 @@ module.exports = function (phantomInstance, file) {
     //   for (z in worksheet) {
     //     /* all keys that do not begin with "!" correspond to cell addresses */
     //     if(z[0] === '!' && JSON.stringify(worksheet[z].v) === 'AF255' ) continue;
-    //     console.log(y + "!" + z + "=" + JSON.stringify(worksheet[z].v));
+    //     //console.log(y + "!" + z + "=" + JSON.stringify(worksheet[z].v));
     //   }
     // });
 
@@ -28,17 +40,57 @@ module.exports = function (phantomInstance, file) {
         var cell_address = XLSX.utils.encode_cell({c:C, r:R});
         var desired_cell = worksheet[cell_address];
         if(desired_cell.v != 'BLANK'){
-            var desired_value = desired_cell.v;
-            console.log(desired_value);
+            var desired_address = desired_cell.v;
+            //console.log(desired_address);
+            var column_two_cell_address = XLSX.utils.encode_cell({c:C+1, r:R});
+            var desired_cell_two = worksheet[column_two_cell_address];
+            if(desired_cell_two.v != 'BLANK'){
+                var desired_address_two = desired_cell_two.v;
+                //console.log(desired_address_two);
+            }
+            // Consider trimming and removing consecutive whitespace later, causes error when called on string for now
+            var cell_url = 'https:\/\/thatsthem.com/advanced-results?d_first=&d_mid=&d_last=&d_email=&d_phone=&d_fulladdr=' + desired_address + '&d_state=&d_city=&d_zip=' + desired_address_two;
+            //console.log(cell_url);
+            // Grab the URLs
+            //console.log('Getting html from: ', cell_url);
+            cachedRequest({ url:cell_url }, function (error, response, body) {
+                var fetched_emails = [];
+                if (!error) {
+                    //console.log('Request successful');
+                    var $ = cheerio.load(body);
+                    var fetched_html = $.html();
+                    // Parse scrambled emails
+                    var emails = fetched_html.replace(/\s+/g, " ").split('var p1 = ');
+                    if(emails.length > 0){
+                        for(var i=1; i < emails.length; i++){
+                            if(emails[i].includes('var p2 = ') && emails[i].includes('var p3 = ') ) {
+                                var email = emails[i].split(';')[0] + emails[i].split('var p2 = ')[1].split(';')[0] + emails[i].split('var p3 = ')[1].split(';')[0];
+                                fetched_emails.push(email.replace(/\"/g,'').replace(/' '/g, ''));
+
+                                //console.log(email.replace(/\"/g,'').replace(/' '/g, ''));
+                            }
+                        }
+                    }
+                    //console.log(fetched_emails.join(';'));
+                    email_column.push(fetched_emails.join(';'));
+                }
+                else {
+                  //console.log("We’ve encountered an error: " + error);
+                }
+            });
         }
         else {
-            
+            // Add placeholder row to align with spreadsheet
+            email_column.push('BLANK');
         }
       }
+      //console.log(email_column);
     }
 
+    for(var i = 0; i < email_column.length;i++){
+        console.log(email_column[i]);
 
-
+    }
 
 
 
---
+++ lib/python/imap-attachments-extractor.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python -tt
+"""
+Author: Mostafa Altantawy
+Email: Mostafa.Altantawy@Gmail.com
+version 0.2
+"""
+import imaplib, email, re
+from ConfigParser import ConfigParser
+
+
+
+config = ConfigParser()
+config.read('config.ini')
+mail_server = config.get('server_config', 'server_hostname')
+user_name = config.get('server_config', 'user_name')
+password = config.get('server_config', 'password')
+files_path = config.get('path', 'files_path')
+urls_path = config.get('path', 'urls_path')
+
+
+class GetEmail():
+	connection = None
+	error = None
+
+	def __init__(self, mail_server, username, password):
+		self.connection = imaplib.IMAP4(mail_server)
+		self.connection.login(username, password)
+		self.connection.select(readonly=False)
+
+
+	def close_connection(self):
+		self.connection.close()
+
+
+	def download_attachments_in_email(self, connection, emailid, outputdir):
+
+		response, data = self.connection.fetch(emailid, "(BODY.PEEK[])")
+		email_body = data[0][1]
+		mail = email.message_from_string(email_body)
+		msg_id = []
+		if mail.get_content_maintype() != 'multipart':
+			return
+		for part in mail.walk():
+			msg_id.append(part['Message-ID'])
+			msg_id = [elem for elem in msg_id if elem != None]
+			msg_id = [elem.replace('<', '') for elem in msg_id]
+			msg_id = [elem.replace('>', '') for elem in msg_id]
+			if part.get_content_maintype() != 'multipart' and part.get('Content-Disposition') is not None:
+				for elem in msg_id:
+					open(outputdir + '/' + elem + '-'+ part.get_filename(), 'wb').write(part.get_payload(decode=True))
+
+	def download_all_attachments_in_inbox(self, server, user, password, outputdir):
+		response, items = self.connection.search(None, "(ALL)")
+		items = items[0].split()
+		for emailid in items:
+			self.download_attachments_in_email(self.connection, emailid, outputdir)
+
+	def extract_url(self):
+		urls_dict = dict()
+		response, items = self.connection.search(None, "(ALL)")
+		items = items[0].split()
+		for emailid in items:
+			response, data = self.connection.fetch(emailid, "(BODY.PEEK[])")
+			email_body = data[0][1]
+			mail = email.message_from_string(email_body)
+			for part in mail.walk():
+				if re.findall(r'(https?://\S+)', str(part)):
+					urls_dict[part['Message-ID']] = re.findall(r'(https?://\S+)', str(part))
+		for k, v in urls_dict.items():
+			urls_dict[k] = ''.join(v)
+
+		urls =  open(str(urls_path) + '/' + 'urls.txt', 'w')
+		for k, v in sorted(urls_dict.items()):
+			urls.write(str(k) +','+str(v) + '\n')
+		urls.close()
+
+
+
+
+imap_connect = GetEmail(mail_server, user_name, password)
+imap_connect.extract_url()
+imap_connect.download_all_attachments_in_inbox(mail_server, user_name, password, files_path)
+imap_connect.close_connection()
